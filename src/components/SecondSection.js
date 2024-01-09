c7c2c8339aa438e58f76bfcd2d7abcee11f99581
sabatermiguelalfonso <sabatermiguel11@gmail.com>
2024-01-09 15:20:20 +0800

    Feat: Added the Hobbies at the About section

--- src/components/SecondSection.js
+++ src/components/SecondSection.js
@@ -23,11 +23,33 @@ const SecondSection = () => {
                     <h2 className="nameContainer"> MIGUEL ALFONSO SABATER</h2>
                     <h3 className="titleContainer"> Graphic Designer | Web Developer </h3>
                     <p className="aboutContentContainer">
-                        A passionate and experienced graphic designer and web developer with a strong background in marketing management. With seven years of expertise in graphic design and three years in web development, I have honed my skills to create visually appealing and functional designs.
+                    I am a skilled graphic designer and web developer with seven years in design and three years in web development. Holding a degree in Business with a major in Marketing Management, I excel in understanding consumer behavior and crafting effective marketing strategies. My expertise extends to brand management, events coordination, and social media management. For ym Hobbies, I enjoy :
                         <br></br>
                         <br></br>
-                        I hold a degree in Business with a major in Marketing Management, which has provided me with a solid foundation in understanding consumer behavior and crafting effective marketing strategies. Alongside my design and development skills, I also possess extensive experience in brand management, events management, and social media management.
-                        <br></br>
+                        <div className="hobbyButtonContainer">
+                            <a href="https://miguelsabater.tech/concept-art/"
+                                target="_blank"
+                                rel="noreferrer">
+                                <button>
+                                    Concept Art
+                                </button>
+                            </a>
+                            <a href="https://miguelsabater.tech/chibi-characters/"
+                                target="_blank"
+                                rel="noreferrer">
+                                <button>
+                                    Chibi Art
+                                </button>
+                            </a>
+                            <a href="https://miguelsabater.tech/digital-painting/"
+                                target="_blank"
+                                rel="noreferrer">
+                                <button>
+                                    Digital Painting
+                                </button>
+                            </a>
+
+                        </div>
                         <br></br>
                         Curious to see my creative journey in action? Delve into my portfolio below, where you'll discover a showcase of sample brands I've created. From crafting memorable logos and building cohesive branding to designing captivating posters and engaging websites, each project tells a unique story. Let these examples inspire your imagination and spark new ideas for our collaboration together.
                     </p>
